dff24618b831b3db2cf9430eb89b4a74c13663a5
raihan tajdid <raihantajdid@gmail.com>
2023-02-13 16:34:08 +0600

    Draw Flower

---
+++ Flowers/main.py
@@ -0,0 +1,17 @@
+import turtle
+
+t = turtle.Turtle()
+s = turtle.Screen()
+s.bgcolor('black')
+t.speed(0)
+
+col = ("yellow", "red", "pink", "cyan", "light green", "blue")
+
+for i in range(150):
+    t.pencolor(col[i % 6])
+    t.circle(190-i / 2, 90)
+    t.lt(90)
+    t.circle(190-i / 3, 90)
+    t.lt(60)
+
+s.exitonclick()
